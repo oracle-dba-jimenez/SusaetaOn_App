18fee621798d642d515b0aa252e45f05ea9e9bf8
Fulvio Moya <fulvio.127@gmail.com>
2018-05-16 22:17:21 -0400

    Designing all screens with buttons

---
+++ app/src/main/java/com/susaeta/susaetaon/library_collection.kt
@@ -0,0 +1,12 @@
+package com.susaeta.susaetaon
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class library_collection : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_library_collection)
+    }
+}
---
+++ app/src/main/java/com/susaeta/susaetaon/serial_code_validator.kt
@@ -0,0 +1,12 @@
+package com.susaeta.susaetaon
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class serial_code_validator : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_serial_code_validator)
+    }
+}
